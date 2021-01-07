1158a7f17a941bb28d530e3e86efb29539e34c2f
Adesegun Koiki <adesegunkoiki202@gmail.com>
2021-01-07 19:14:51 +0100

    End of Section 3

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Skinet';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+      this.http.get('https://local')
+  }
 }
