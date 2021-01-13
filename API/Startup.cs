3506e71f93a0982e6a9cf1d909d8650849060850
Adesegun Koiki <adesegunkoiki202@gmail.com>
2021-01-13 11:46:37 +0100

    End of Section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,7 @@
 
+using API.Helpers;
+using AutoMapper;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -23,7 +26,9 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+            services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
             services.AddSwaggerGen(c =>
@@ -45,6 +50,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseStaticFiles();
 
             app.UseAuthorization();
 
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   title = 'Skinet';
+  products: IProduct[];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-      this.http.get('https://local')
+      this.http.get('https://localhost:5001/api/products').subscribe((response: any) => {
+        this.products = response.data; 
+      }, error => {
+        console.log(error);
+        
+      })
   }
 }
