4f4b359472f9917bbf69510c59ea23058bc870c8
Adesegun Koiki <kamar3deen@gmail.com>
2024-06-06 10:21:30 -0600

    updated to angular 12

--- API/Dtos/orderDto.cs
+++
@@ -1,9 +0,0 @@
-namespace API.Dtos
-{
-    public class orderDto
-    {
-        public string BasketId { get; set; }
-        public int DeliveryMethodId { get; set; }
-        public AddressDto ShipToAddress { get; set; }
-    }
-}
--- client/README.md
+++ client/README.md
@@ -1,3 +1,7 @@
+# SSL
+
+install mkcert
+
 # Client
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.26.
