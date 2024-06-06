bc6aed23e3addcb530de0f6ca99d4f9bfa688202
Adesegun Koiki <kamar3deen@gmail.com>
2024-06-06 10:39:11 -0600

    updated to angular 14

--- client/src/app/shared/models/address.ts
+++
@@ -1,9 +0,0 @@
-export interface IAddress {
-    id:        number;
-    firstName: string;
-    lastName:  string;
-    street:    string;
-    city:      string;
-    state:     string;
-    zipCode:   string;
-}
--- client/src/app/shared/models/pagination.ts
+++
@@ -1,8 +0,0 @@
-import { IProduct } from "./product";
-
-export interface IPagniation {
-    pageIndex: number;
-    pageSize: number;
-    count: number;
-    data: IProduct[]
-}
